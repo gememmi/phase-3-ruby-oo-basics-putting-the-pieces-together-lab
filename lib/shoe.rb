b178b96c37055b1a5e1b78f1c948d27050ab66b6
Emily <emelizadaniels@gmail.com>
2022-11-22 19:45:49 -0500

    commit tree 4f4df118f384d91a92eb4333a9537ace59692fc5

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand, :color, :size , :material, :condition
+    
+    def initialize(brand)
+        @brand= brand
+        @color= color
+        @size= size
+        @material= material
+        @condition= condition
+    end
+    def cobble
+       puts "Your shoe is as good as new!"
+       @condition = "new"
+    end
+end
+s1 = Shoe.new("Nike")
+# s1.color= "red"
+# s1.size= 9.5
+# s1.material= "cotton"
+# s1.condtion= "okay"
+# binding.pry
